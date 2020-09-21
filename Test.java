8b6ded071448e46d0fdccafb5db58c9adf72ac4a
DEV <DEV@gmail.com>
2020-09-21 14:21:30 +0530

    CR-234

--- Test.java
+++ Test.java
@@ -1,8 +1,15 @@
 class Test{
    
      public static void main(String... args){
-        int x=90;
-         System.out.ppritln("Git prac-II");
+         int x=90;
+         int y=980;
+         int total= sum(x,y);
+         System.out.ppritln("Git prac-II"+total);
       }
+
+      //CR-234
+      public static int sum(int x,int y){
+            return x+y;
+   }
 
 }
